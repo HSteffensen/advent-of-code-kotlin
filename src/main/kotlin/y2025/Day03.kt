e095db536c54b3f2e70b856a663629dba4ae7c44
Henry Steffensen <hsteffensen@bol.com>
2025-12-03 10:12:32 +0100

    2025 day 3 part 2

--- src/main/kotlin/y2025/Day03.kt
+++ src/main/kotlin/y2025/Day03.kt
@@ -10,8 +10,8 @@ fun main(): Unit =
             val input = readInput(DAY)
             assert(solve1(parseInput(EXAMPLE_1)) == ANSWER_1) { "Example 1 failed" }
             println(solve1(parseInput(input)))
-//            assert(solve2(parseInput(EXAMPLE_2)) == ANSWER_2) { "Example 2 failed" }
-//            println(solve2(parseInput(input)))
+            assert(solve2(parseInput(EXAMPLE_1)) == ANSWER_2) { "Example 2 failed" }
+            println(solve2(parseInput(input)))
         }
     }
 
@@ -20,20 +20,30 @@ object Day3 {
 
     fun parseInput(input: String): List<List<Int>> = input.trim().lines().map { line -> line.toCharArray().map { it.digitToInt() } }
 
-    fun solve1(input: List<List<Int>>): String = input.sumOf { maxJoltageForBank(it) }.toString()
+    fun solve1(input: List<List<Int>>): String = input.sumOf { maxJoltageForBankPart1(it) }.toString()
 
-    fun maxJoltageForBank(batteries: List<Int>): Int {
-        var max = 0
-        for ((i, v) in batteries.dropLast(1).withIndex()) {
-            val joltage = (10 * v) + batteries.subList(i + 1, batteries.size).max()
-            max = max(max, joltage)
+    fun maxJoltageForBankPart1(batteries: List<Int>): Int =
+        batteries.dropLast(1).withIndex().fold(0) { maxSoFar, (i, v) ->
+            max(maxSoFar, (10 * v) + batteries.subList(i + 1, batteries.size).max())
         }
-        return max
-    }
 
-    fun solve2(input: List<List<Int>>): String {
-        TODO()
-    }
+    fun solve2(input: List<List<Int>>): String = input.sumOf { maxJoltageForBankPart2(it) }.toString()
+
+    fun maxJoltageForBankPart2(batteries: List<Int>): Long =
+        batteries.fold(mapOf(0 to 0L)) { maxSoFar, v ->
+            maxJoltageForBankPart2Helper(maxSoFar, v)
+        }[12]!!
+
+    fun maxJoltageForBankPart2Helper(
+        largestPerSize: Map<Int, Long>,
+        nextBattery: Int,
+    ): Map<Int, Long> =
+        (largestPerSize + mapOf(largestPerSize.keys.max() + 1 to 0)).mapValues { (key, existingValue) ->
+            if (key == 0) return@mapValues 0
+            val appendDigit = (largestPerSize[key - 1]!! * 10) + nextBattery
+            val replaceLastDigit = ((existingValue / 10) * 10) + nextBattery
+            max(existingValue, max(appendDigit, replaceLastDigit))
+        }
 
     const val EXAMPLE_1 = """987654321111111
 811111111111119
@@ -43,7 +53,5 @@ object Day3 {
 
     const val ANSWER_1 = "357"
 
-    const val EXAMPLE_2 = """"""
-
-    const val ANSWER_2 = ""
+    const val ANSWER_2 = "3121910778619"
 }
